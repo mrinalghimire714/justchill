2ee950f11b6e8ea83806a4a20d6f72fcd5fa9922
Mrinal <mrinal.ghimire714@gmail.com>
2026-09-07 20:11:45 +0545

    cdd

--- backend/test_app.py
+++ backend/test_app.py
@@ -7,15 +7,18 @@ def client():
     return app.test_client()
 
 def test_home_route(client):
-    response = client.get('/dog')
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"welcome to the Public API Demo" in response.data
+
+def test_dog_route(client):
+    response = client.get("/dog")
     assert response.status_code == 200
     data = response.get_json()
     assert "image_url" in data
     assert data["status"] == "success"
 
 def test_dog_html_route(client):
-    response = client.get("/dog.html")
+    response = client.get("/dog/html")
     assert response.status_code == 200
-    assert b "<h1>Random Dog</h1>" in response.data
-    assert b "<img src='' in response.data
-
+    assert b"<h1>Random Dog</h1>" in response.data
